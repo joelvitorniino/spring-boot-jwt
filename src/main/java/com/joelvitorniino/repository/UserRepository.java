cd2e7ac3b4ece4ae41d7c0931e975ef81823dba5
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 09:04:06 -0300

    findByUsername on user repository

--- src/main/java/com/joelvitorniino/repository/UserRepository.java
+++ src/main/java/com/joelvitorniino/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.joelvitorniino.repository;
 import com.joelvitorniino.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByUsername(String username);
 }
