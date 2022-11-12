2091bfb5c70e189825d457ae6577c85f8ae4cf7d
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 09:32:46 -0300

    method getPasswordEncoder on index of application

--- src/main/java/com/joelvitorniino/SpringBootJwtApplication.java
+++ src/main/java/com/joelvitorniino/SpringBootJwtApplication.java
@@ -3,6 +3,9 @@ package com.joelvitorniino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringBootJwtApplication {
@@ -11,4 +14,8 @@ public class SpringBootJwtApplication {
 		SpringApplication.run(SpringBootJwtApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
