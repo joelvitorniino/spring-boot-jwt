1cc0b5d150eb6a7d79f7b2aff1fdcc6da228e9c8
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 08:12:52 -0300

    User not found exception

---
+++ src/main/java/com/joelvitorniino/service/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joelvitorniino.service.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String msg) {
+        super(msg);
+    }
+}
