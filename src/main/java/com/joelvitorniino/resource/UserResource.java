fc0ada8aaf8b64682e7e51350b5436bea8485980
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 08:40:11 -0300

    deleteById on user endpoint

--- src/main/java/com/joelvitorniino/resource/UserResource.java
+++ src/main/java/com/joelvitorniino/resource/UserResource.java
@@ -42,4 +42,11 @@ public class UserResource {
 
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
+        service.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
