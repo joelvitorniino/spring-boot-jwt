287cd5eff48104773854294efddd3538be072fa6
joelvitorniino <joelprofessor13@gmail.com>
2022-11-12 08:15:53 -0300

    deleteById and findById on user service

--- src/main/java/com/joelvitorniino/service/UserService.java
+++ src/main/java/com/joelvitorniino/service/UserService.java
@@ -2,10 +2,12 @@ package com.joelvitorniino.service;
 
 import com.joelvitorniino.model.User;
 import com.joelvitorniino.repository.UserRepository;
+import com.joelvitorniino.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +20,20 @@ public class UserService {
 
     public User insert(User obj) {
         return repository.save(obj);
+    }
+
+    public void deleteById(Integer id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public Optional<User> findById(Integer id) {
+        Optional<User> user = repository.findById(id);
+
+        if(user.isPresent()) {
+            return user;
+        } else {
+            user.orElseThrow(() -> new UserNotFoundException("User is not found"));
+        }
     }
 }
