eb8edc3d10a4787070c48f65fcb86d4cb5e0880a
joelvitorniino <joelprofessor13@gmail.com>
2022-11-13 06:40:02 -0300

    Create DetailUserData to jwt

---
+++ src/main/java/com/joelvitorniino/data/DetailUserData.java
@@ -0,0 +1,52 @@
+package com.joelvitorniino.data;
+
+import com.joelvitorniino.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+public class DetailUserData  implements UserDetails {
+    private final Optional<User> user;
+
+    public DetailUserData(Optional<User> user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.orElse(new User()).getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.orElse(new User()).getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
